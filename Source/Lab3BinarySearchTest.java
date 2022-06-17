7bd769a82a17f628cc240462dad0c00c0b4f06d9
aaronDykes <88196527+aaronDykes@users.noreply.github.com>
2022-06-17 12:34:54 -0400

    Format fix from initial push

--- Source/Lab3BinarySearchTest.java
+++ Source/Lab3BinarySearchTest.java
@@ -49,48 +49,48 @@ public class Lab3BinarySearchTest {
 				switch ( option ) {
 				
 					case 1: search.generateRandomIntegers( data,
-														   0,
-														   data.length - 1 );
-							break;
+									       0,
+									       data.length - 1 );
+						break;
 					case 2: System.out.print( integerPrompt );
-							key = input.nextInt();
-							input.nextLine();
-							search.recursiveBinarySearch( data, 
-													  	  0, 
-													  	  data.length - 1, 
-													  	  key );
-							break;
+						key = input.nextInt();
+						input.nextLine();
+						search.recursiveBinarySearch( data, 
+									      0, 
+									      data.length - 1, 
+									      key );
+						break;
 					case 3: System.out.print( integerPrompt );
-							key = input.nextInt();
-							input.nextLine();
-							search.nonRecursiveBinarySearch( data, 
-														 	 key );
-							break;
+						key = input.nextInt();
+						input.nextLine();
+						search.nonRecursiveBinarySearch( data, 
+										 key );
+						break;
 					case 4: goodInput = false;
-							System.out.println("System exiting.");
-							break;
+						System.out.println("System exiting.");
+						break;
 				
 					default: System.out.printf( "%n%s%n%s%n",
 								    "Sorry, that's an invalid input.",
 								    "Please try again." );
 				}
-				
-			} catch( InputMismatchException ime ) {
-					 System.out.printf( "%n%s%n%s%n",
-							    "Sorry, that's an invalid data type.",
-						  	    "Please try again." );
-					 input.nextLine();
+			} 
+			catch( InputMismatchException ime ) {
+			       System.out.printf( "%n%s%n%s%n",
+						  "Sorry, that's an invalid data type.",
+						  "Please try again." );
+			       input.nextLine();
 			}
 		} input.close();
 	}
 	
 	private static void printMenu() {
 		System.out.printf( "%n%n%s%n%s%n%s%n%s%n%s",
-						   "1. Initialize and populate an array of 20 random integers.",
-						   "2. Perform a recursive binary search.",
-				 		   "3. Perform a non-recursive binary search.",
-						   "4. Exit.",
-				 		   "Select your option in the menu below: ");
+				   "1. Initialize and populate an array of 20 random integers.",
+				   "2. Perform a recursive binary search.",
+				   "3. Perform a non-recursive binary search.",
+				   "4. Exit.",
+				   "Select your option in the menu below: ");
 	}
 }
 /*
